77b8b528ebcc0be06ca31434109c3a85bc2382cc
CuriousPersona <96737538+CuriousPersona@users.noreply.github.com>
2024-01-08 11:06:39 +0530

    Delete courseInfo directory

--- courseInfo/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .forms import CourseDetailsForm
-
-# Create your views here.
-def django_courseInfo(request):
-    display = {"cI_form" : CourseDetailsForm()}
-
-    return render(request, 'courseInfo/courseInfo.html', context = display)
